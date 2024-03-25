b27bae150370c658546604c9c32191e9cb475dcf
Masumi Yano <masumiyano0706@gmail.com>
2024-03-24 23:59:09 -0700

    models created

---
+++ models.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import os
+import datetime
+import pdb
+import time
+import torchvision.models as torchmodels
+
+
+class BaseModel(nn.Module):
+    def __init__(self):
+        super(BaseModel, self).__init__()
+        super(BaseModel, self).__init__()
+        if not os.path.exists('logs'):
+            os.makedirs('logs')
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H-%M-%S_log.txt')
+        self.logFile = open('logs/' + st, 'w')
+
+    def log(self, str):
+        print(str)
+        self.logFile.write(str + '\n')
+
+    def criterion(self):
+        return nn.CrossEntropyLoss()
+
+    def optimizer(self):
+        return optim.SGD(self.parameters(), lr=0.001)
+
+    def adjust_learning_rate(self, optimizer, epoch, args):
+        lr = args.lr  # TODO: Implement decreasing learning rate's rules
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+
+
+class LNET(BaseModel):
+    def __init__(self):
+        super(LNET, self).__init__()
+        # self.conv1 = nn.Conv2d()
